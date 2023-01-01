fb1b23df449b4cb1092c6db608440388fac5373b
YTNulls memory <grigoriyluben@gmail.com>
2023-01-01 14:32:00 +0200

    first commit

---
+++ project/Мої уроки/index.js
@@ -0,0 +1,20 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) 
+let slides = document.getElementsByClassName("mySlides") {
+    let dots = document.getElementsByClassName("dot")
+    if (n > slides.length) (slideindex - i)
+    if (n < 1) (slideIndex = slides.length)
+    for (i - 0; 1 < slides.length; i++) 
+        slides{ 1 }.style.display = "none"; 
+}
+alert("123")
